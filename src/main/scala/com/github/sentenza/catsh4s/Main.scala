b05633639f6090dbc0002a4b45be43fd55d24d8e
Alfredo Torre <alfredotorre@gmail.com>
2021-10-13 20:54:05 +0200

    Remove fs2 stream

--- src/main/scala/com/github/sentenza/catsh4s/Main.scala
+++ src/main/scala/com/github/sentenza/catsh4s/Main.scala
@@ -1,8 +1,8 @@
 package com.github.sentenza.catsh4s
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{IO, IOApp}
 import com.github.sentenza.catsh4s.infrastructure.Server
 
-object Main extends IOApp {
-  def run(args: List[String]): IO[ExitCode] = Server.stream[IO].compile.drain.as(ExitCode.Success)
+object Main extends IOApp.Simple {
+  override def run: IO[Unit] = Server.run[IO]
 }
--- src/main/scala/com/github/sentenza/catsh4s/infrastructure/Server.scala
+++ src/main/scala/com/github/sentenza/catsh4s/infrastructure/Server.scala
@@ -1,12 +1,10 @@
 package com.github.sentenza.catsh4s.infrastructure
 
-import cats.effect.{Async, Resource}
-import cats.implicits.catsSyntaxFlatMapOps
+import cats.effect.Async
 import com.comcast.ip4s._
 import com.github.sentenza.catsh4s.config.AppConfig
 import com.github.sentenza.catsh4s.infrastructure.routes.{CoinMarketCapRoutes, HealthRoutes, MainApiRoutes}
 import com.github.sentenza.catsh4s.infrastructure.service.{CoinMarketCapService, PingService}
-import fs2.Stream
 import org.http4s.HttpApp
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
@@ -14,11 +12,11 @@ import org.http4s.server.middleware.Logger
 
 object Server {
 
-  def stream[F[_]: Async]: Stream[F, Nothing] = {
+  def run[F[_]: Async]: F[Nothing] = {
     import cats.syntax.semigroupk._
-    for {
-      client        <- Stream.resource(EmberClientBuilder.default[F].build)
-      generalConfig <- Stream.resource(AppConfig.load())
+    (for {
+      client        <- EmberClientBuilder.default[F].build
+      generalConfig <- AppConfig.load()
       pingService    = PingService.impl[F]
       cmcService     = CoinMarketCapService.impl[F](client, generalConfig.cmc)
       mainApiRoutes  = MainApiRoutes.essentialRoutes[F](pingService)
@@ -30,7 +28,7 @@ object Server {
       // Middlewares
       finalHttpApp: HttpApp[F] = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
 
-      exitCode <- Stream.resource(
+      _ <-
         EmberServerBuilder
           .default[F]
           .withHost(
@@ -40,9 +38,8 @@ object Server {
           )
           .withPort(Port.fromInt(generalConfig.server.port).getOrElse(port"8080"))
           .withHttpApp(finalHttpApp)
-          .build >>
-          Resource.eval(Async[F].never)
-      )
-    } yield exitCode
-  }.drain
+          .build
+
+    } yield ()).useForever
+  }
 }
